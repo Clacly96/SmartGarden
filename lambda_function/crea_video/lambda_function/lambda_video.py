1c0ea20750f0fee1a2476b3eccd1d88e71c08921
Roberto Broccoletti <roby.brokk.96@gmail.com>
2018-11-04 20:24:39 +0100

    modifiche alle lambda function

--- lambda_function/crea_video/lambda_function/lambda_video.py
+++ lambda_function/crea_video/lambda_function/lambda_video.py
@@ -69,18 +69,18 @@ def crea_video(inizio,fine,percorso,fps,grayscale,frame_width,frame_height,info_
                 with open('/tmp/{}'.format(nome_foto), 'wb') as data:
                     s3.download_fileobj(nome_bucket, oggetto['Key'], data)
                 vettore_file_data[data_dt]='/tmp/{}'.format(nome_foto)
-                print('[TEST] Foto {} caricata. Con key {}'.format(nome_foto,oggetto['Key']))
+                #print('[TEST] Foto {} caricata. Con key {}'.format(nome_foto,oggetto['Key']))
         except KeyError:
             #inserire eventualmente il log
             pass
-
+    ultimo=""
     for key in sorted(vettore_file_data):
         if grayscale==1:
             img=cv2.imread(vettore_file_data[key],cv2.IMREAD_GRAYSCALE)
             img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
         elif grayscale==0:
             img=cv2.imread(vettore_file_data[key],cv2.IMREAD_ANYCOLOR)
-            print('[TEST] File aperto {}'.format(vettore_file_data[key]))
+            #print('[TEST] File aperto {}'.format(vettore_file_data[key]))
         # shape restituisce (altezza,larghezza,num_canali)
         #calcolo nuove dimensioni
         if img.shape[0]!=frame_height or img.shape[1]!=frame_width:
@@ -92,77 +92,76 @@ def crea_video(inizio,fine,percorso,fps,grayscale,frame_width,frame_height,info_
         altezza_rect=dimtesto[1]+10
         cv2.rectangle(frame,(0,frame_height),(frame_width,frame_height-altezza_rect),(0,0,0),-1)
         cv2.putText(frame,str(key),(int((frame_width-dimtesto[0])/2),int(frame_height-(altezza_rect-dimtesto[1])/2)),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-
-
         out.write(frame)
-
+        ultimo=key
 
     out.release()
+    return vettore_file_data[ultimo]
 
-def crea_video_h264(inizio,fine,percorso,fps,grayscale,frame_width,frame_height,info_video,s3,nome_bucket): #fa il video a partire dalle foto che sono state modificate in colore da opencv
-    if percorso[-1]!="/":
-        percorso=percorso+"/"
-
-    inizio_dt=datetime.datetime.strptime(inizio,"%Y-%m-%d")
-    fine_dt=datetime.datetime.strptime(fine,"%Y-%m-%d")
-
-    vettore_file_data={}
-    lista_oggetti=s3.list_objects(Bucket=nome_bucket,Prefix=percorso)['Contents']
-    #file dove andranno scritti nell'ordine corretto i path dei file da far caricare da ffmpeg per la creazione del video
-
-    for oggetto in lista_oggetti:
-        try:
-            data=s3.get_object(Bucket='ortobotanico',Key=oggetto['Key'])['Metadata']['data_creazione']
-            data_dt=datetime.datetime.strptime(data,"%Y:%m:%d %H:%M:%S")
-            if data_dt>=inizio_dt and data_dt<=fine_dt:
-                nome_foto=oggetto['Key'].split("/")[-1]
-                vettore_file_data[data_dt]='/tmp/{}'.format(nome_foto)
-                with open(vettore_file_data[data_dt], 'wb') as data:
-                    s3.download_fileobj(nome_bucket, oggetto['Key'], data)
-
-                #print('[TEST] Foto {} caricata. Con key {}'.format(nome_foto,oggetto['Key']))
-
-        except KeyError:
-            #inserire eventualmente il log
-            pass
-        except Exception as e:
-            print(e)
-
-
-    ffmpeg_file=open("/tmp/input_ffmpeg.txt","w")
-    ultimo=0
-    for key in sorted(vettore_file_data):
-        if grayscale==1:
-            img=cv2.imread(vettore_file_data[key],cv2.IMREAD_GRAYSCALE) #apre l'immagine convertita in bianco e nero
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) #necessario riconvertire perchè opencv per scrivere il video parte dai tre canali colore, sennò non li avrebbe. anche se poi effettivamente l'immagine rimane in BW
-        else:
-            img=cv2.imread(vettore_file_data[key],cv2.IMREAD_ANYCOLOR)
-        # shape restituisce (altezza,larghezza,num_canali)
-        #calcolo nuove dimensioni
-        if img.shape[0]!=frame_height or img.shape[1]!=frame_width:
-            frame=ridimensiona(img,frame_height,frame_width)
-        else:
-            frame=img
-        # Aggiunta della data
-        dimtesto=cv2.getTextSize(str(key),cv2.FONT_HERSHEY_SIMPLEX,1,1)[0] # (larghezza,altezza)
-        altezza_rect=dimtesto[1]+10
-        cv2.rectangle(frame,(0,frame_height),(frame_width,frame_height-altezza_rect),(0,0,0),-1)
-        cv2.putText(frame,str(key),(int((frame_width-dimtesto[0])/2),int(frame_height-(altezza_rect-dimtesto[1])/2)),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1,cv2.LINE_AA)
-        # sovrascriviamo l'immagine
-        cv2.imwrite(vettore_file_data[key],frame)
-        ffmpeg_file.write("file {} \n".format(vettore_file_data[key])) # inserisce il nome del file da concatenare
-        ffmpeg_file.write("duration {}\n".format(str(1/fps))) #la durata che deve avere quel file nel video, cioè 1/fps
-        ultimo=key
-    ffmpeg_file.write("file {} \n".format(vettore_file_data[ultimo]))   #l'ultimo file va ripetuto due volte, la seconda volta dopo la sua durata
-    ffmpeg_file.close()
-    path_out='/tmp/{}.mp4'.format(info_video['nome'])
-
-    comando="ffmpeg -f concat -safe 0 -i {0} -vsync vfr -pix_fmt yuv420p {1}".format("/tmp/input_ffmpeg.txt",path_out)
-    try:
-        subprocess.call(comando.split()) # usa split per passargli la stringa di comando come tupla
-    except Exception as e:
-        print(e)
-    return path_out
+# def crea_video_h264(inizio,fine,percorso,fps,grayscale,frame_width,frame_height,info_video,s3,nome_bucket): #fa il video a partire dalle foto che sono state modificate in colore da opencv
+#     if percorso[-1]!="/":
+#         percorso=percorso+"/"
+#
+#     inizio_dt=datetime.datetime.strptime(inizio,"%Y-%m-%d")
+#     fine_dt=datetime.datetime.strptime(fine,"%Y-%m-%d")
+#
+#     vettore_file_data={}
+#     lista_oggetti=s3.list_objects(Bucket=nome_bucket,Prefix=percorso)['Contents']
+#     #file dove andranno scritti nell'ordine corretto i path dei file da far caricare da ffmpeg per la creazione del video
+#
+#     for oggetto in lista_oggetti:
+#         try:
+#             data=s3.get_object(Bucket='ortobotanico',Key=oggetto['Key'])['Metadata']['data_creazione']
+#             data_dt=datetime.datetime.strptime(data,"%Y:%m:%d %H:%M:%S")
+#             if data_dt>=inizio_dt and data_dt<=fine_dt:
+#                 nome_foto=oggetto['Key'].split("/")[-1]
+#                 vettore_file_data[data_dt]='/tmp/{}'.format(nome_foto)
+#                 with open(vettore_file_data[data_dt], 'wb') as data:
+#                     s3.download_fileobj(nome_bucket, oggetto['Key'], data)
+#
+#                 #print('[TEST] Foto {} caricata. Con key {}'.format(nome_foto,oggetto['Key']))
+#
+#         except KeyError:
+#             #inserire eventualmente il log
+#             pass
+#         except Exception as e:
+#             print(e)
+#
+#
+#     ffmpeg_file=open("/tmp/input_ffmpeg.txt","w")
+#     ultimo=0
+#     for key in sorted(vettore_file_data):
+#         if grayscale==1:
+#             img=cv2.imread(vettore_file_data[key],cv2.IMREAD_GRAYSCALE) #apre l'immagine convertita in bianco e nero
+#             img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) #necessario riconvertire perchè opencv per scrivere il video parte dai tre canali colore, sennò non li avrebbe. anche se poi effettivamente l'immagine rimane in BW
+#         else:
+#             img=cv2.imread(vettore_file_data[key],cv2.IMREAD_ANYCOLOR)
+#         # shape restituisce (altezza,larghezza,num_canali)
+#         #calcolo nuove dimensioni
+#         if img.shape[0]!=frame_height or img.shape[1]!=frame_width:
+#             frame=ridimensiona(img,frame_height,frame_width)
+#         else:
+#             frame=img
+#         # Aggiunta della data
+#         dimtesto=cv2.getTextSize(str(key),cv2.FONT_HERSHEY_SIMPLEX,1,1)[0] # (larghezza,altezza)
+#         altezza_rect=dimtesto[1]+10
+#         cv2.rectangle(frame,(0,frame_height),(frame_width,frame_height-altezza_rect),(0,0,0),-1)
+#         cv2.putText(frame,str(key),(int((frame_width-dimtesto[0])/2),int(frame_height-(altezza_rect-dimtesto[1])/2)),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1,cv2.LINE_AA)
+#         # sovrascriviamo l'immagine
+#         cv2.imwrite(vettore_file_data[key],frame)
+#         ffmpeg_file.write("file {} \n".format(vettore_file_data[key])) # inserisce il nome del file da concatenare
+#         ffmpeg_file.write("duration {}\n".format(str(1/fps))) #la durata che deve avere quel file nel video, cioè 1/fps
+#         ultimo=key
+#     ffmpeg_file.write("file {} \n".format(vettore_file_data[ultimo]))   #l'ultimo file va ripetuto due volte, la seconda volta dopo la sua durata
+#     ffmpeg_file.close()
+#     path_out='/tmp/{}.mp4'.format(info_video['nome'])
+#
+#     comando="ffmpeg -f concat -safe 0 -i {0} -vsync vfr -pix_fmt yuv420p {1}".format("/tmp/input_ffmpeg.txt",path_out)
+#     try:
+#         subprocess.call(comando.split()) # usa split per passargli la stringa di comando come tupla
+#     except Exception as e:
+#         print(e)
+#     return path_out
 
 
 def converti_video(path_in,path_out):
@@ -189,39 +188,38 @@ def handler(event, context):
             s3_client.download_fileobj(nome_bucket, path_oggetto, data)
         with open(download_path, 'r') as f:
             config = json.load(f)
-        '''crea_video(
-                    str(config['data_inizio']),
-                    str(config['data_fine']),
-                    str(config['percorso']),
-                    int(config['fps']),
-                    int(config['grayscale']),
-                    int(config['frame_width']),
-                    int(config['frame_height']),
-                    dict(config['info_video']), #da riconsiderare come fare le info video dato che il nome è legato alla singola piantina quindi univoco e non basta un singolo file di config in quel modo sennò
-                    s3_client,
-                    nome_bucket
-                    )'''
-        #video_path_avi = '/tmp/{}.avi'.format(config['info_video']['nome'])
-        #video_path_dest = '/tmp/{0}.{1}'.format(config['info_video']['nome'],config['info_video']['estensione'])
-        #converti_video(video_path_avi,video_path_dest)
-        # da testare entrambe
-        video_path_dest = crea_video_h264(
-                                            str(config['data_inizio']),
-                                            str(config['data_fine']),
-                                            str(config['percorso']),
-                                            int(config['fps']),
-                                            int(config['grayscale']),
-                                            int(config['frame_width']),
-                                            int(config['frame_height']),
-                                            dict(config['info_video']), #da riconsiderare come fare le info video dato che il nome è legato alla singola piantina quindi univoco e non basta un singolo file di config in quel modo sennò
-                                            s3_client,
-                                            nome_bucket
-                                            )
+        # thumbnail è il path dell'immagine che verrà usata come thumbnail, cioè l'ultima
+        thumbnail= crea_video(
+                                str(config['data_inizio']),
+                                str(config['data_fine']),
+                                str(config['percorso']),
+                                int(config['fps']),
+                                int(config['grayscale']),
+                                int(config['frame_width']),
+                                int(config['frame_height']),
+                                dict(config['info_video']), #da riconsiderare come fare le info video dato che il nome è legato alla singola piantina quindi univoco e non basta un singolo file di config in quel modo sennò
+                                s3_client,
+                                nome_bucket
+                                )
+        video_path_avi = '/tmp/{}.avi'.format(config['info_video']['nome'])
+        video_path_dest = '/tmp/{0}.{1}'.format(config['info_video']['nome'],config['info_video']['estensione'])
+        converti_video(video_path_avi,video_path_dest)
 
+        with open(thumbnail, 'rb') as video:
+            s3_client.upload_fileobj(video,
+                                    nome_bucket,
+                                    'demo/video/pianta1/thumbnail/{}.jpg'.format(config['info_video']['nome']),
+                                    ExtraArgs={
+                                        "ACL": "public-read",
+                                        "Metadata": {
+                                            "height": str(config['frame_height']),
+                                            "width":str(config['frame_width'])
+                                            }
+                                        })
         with open(video_path_dest, 'rb') as video:
             s3_client.upload_fileobj(video,
                                     nome_bucket,
-                                    'test2/OUT/{0}.{1}'.format(config['info_video']['nome'],config['info_video']['estensione']),
+                                    'demo/video/pianta1/{0}.{1}'.format(config['info_video']['nome'],config['info_video']['estensione']),
                                     ExtraArgs={
                                         "ACL": "public-read",
                                         "Metadata": {
--- lambda_function/playercard/lambda_function/lambda_function.py
+++ lambda_function/playercard/lambda_function/lambda_function.py
@@ -1,21 +1,23 @@
 import boto3,json
 from jinja2 import Environment, FileSystemLoader,select_autoescape
 
-path_twittercard="test2/card_twitter/"
+local_player="/tmp/player.html"
+local_card="/tmp/card.html"
+path_twittercard="demo/card/"
 
 env = Environment(
         loader=FileSystemLoader("/tmp"),
         autoescape=select_autoescape(['html']),
         trim_blocks=True
     )
-def html_player(video_url):
-    player_temp=env.get_template("temp_player.html")
+def html_player(video_url,template):
+    player_temp=env.get_template(template)
     player=player_temp.render(video_url=video_url)
     return player
 
-def html_card(title,description,image_url,player_url,video_width,video_height):
-    
-    card_temp=env.get_template("temp_card.html")
+def html_card(title,description,image_url,player_url,video_width,video_height,template):
+
+    card_temp=env.get_template(template)
     card=card_temp.render(title=title,description=description,image_url=image_url,player_url=player_url,video_height=video_height,video_width=video_width)
     return card
 
@@ -24,9 +26,10 @@ def handler(event, context):
     s3_client = boto3.client('s3')
     for record in event['Records']:
         nome_bucket = record['s3']['bucket']['name']
-        path_video = record['s3']['object']['key'] 
-        
-
+        path_video = record['s3']['object']['key']
+        nome_pianta = path_video.split('/')[-2]
+        nome_video = path_video.split('/')[-1]
+        nome_video=nome_video.split('.')[0]
         #download config file
         with open("/tmp/config.json", 'wb') as data:
             s3_client.download_fileobj(nome_bucket, path_twittercard+"config.json", data)
@@ -38,25 +41,25 @@ def handler(event, context):
             s3_client.download_fileobj(nome_bucket, path_twittercard+"template/card.html", data)
         with open("/tmp/temp_player.html", 'wb') as data:
             s3_client.download_fileobj(nome_bucket, path_twittercard+"template/player.html", data)
-        
+
         # Crea html player e lo inserisce su s3
         url_video='https://s3.amazonaws.com/{}/{}'.format(nome_bucket,path_video)
-        
+        key_player=path_twittercard+'{}/player-{}.html'.format(nome_pianta,nome_video)
         s3_client.put_object(
             Bucket=nome_bucket,
-            Body=html_player(url_video),
-            Key=path_twittercard+'player-{}.html'.format(int(config['id_pianta'])),
+            Body=html_player(url_video,"temp_player.html"),
+            Key=key_player,
             ACL='public-read',
             ContentType='text/html'
         )
         # Crea html card e lo carica su s3
         videoobj=s3_client.get_object(Bucket=nome_bucket,Key=path_video)['Metadata']
-        url_player='https://s3.amazonaws.com/{}/{}'.format(nome_bucket,path_twittercard+'player-{}.html'.format(int(config['id_pianta'])))
-        
+        url_player='https://s3.amazonaws.com/{}/{}'.format(nome_bucket,key_player)
+        key_card=path_twittercard+'{}/card-{}.html'.format(nome_pianta,nome_video)
         s3_client.put_object(
             Bucket=nome_bucket,
-            Body=html_card(config['titolo'],config['descrizione'],config['url_immagine'],url_player,videoobj['width'],videoobj['height']),
-            Key=path_twittercard+'card-{}.html'.format(int(config['id_pianta'])),
+            Body=html_card(config['titolo'],config['descrizione'],config['url_immagine'],url_player,videoobj['width'],videoobj['height'],"temp_card.html"),
+            Key=key_card,
             ACL='public-read',
             ContentType='text/html'
         )
