ffb1c6b5b57d1675c28ba60651acd9e99760ca25
Claudio Spada <claudiospada@outlook.it>
2018-12-04 23:39:12 +0100

    upload lambda function

---
+++ lambda_function/APIGateway_store_DB/lambda_function.py
@@ -0,0 +1,31 @@
+import boto3
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('devicedata')
+
+    #get all dendrometer data from event
+    dendrometers_data=dict()
+    for key in event:
+        if 'dendrometerCh' in key:
+            dendrometers_data[key]=str(event[key])
+    item={
+        "deviceUUID": str(event['deviceUUID']),
+        "timestamp": event['timestamp'],
+        "temperature": str(event['temperature']),
+        "umidity": str(event['umidity']),
+    }
+    #add dendrometer data to item dict
+    item.update(dendrometers_data)
+    #insert data into the table
+    resp=table.put_item(Item=item)
+
+    if resp['ResponseMetadata']['HTTPStatusCode']==200:
+        return {
+            'statusCode': 200,
+            'body': 'Data entered successfully'
+        }
+    else:
+        return{
+            'body': 'There was an error'
+        }
